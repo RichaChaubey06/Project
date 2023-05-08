187986ba7e367091e6ccfa027eae8ed51e53f8ca
RichaChaubey06 <132867538+RichaChaubey06@users.noreply.github.com>
2023-05-08 09:58:46 +0530

    changes done (#1)

--- src/main/java/com/jsp/SpringBoot/config/SpringBootConfig.java
+++ src/main/java/com/jsp/SpringBoot/config/SpringBootConfig.java
@@ -3,5 +3,6 @@ package com.jsp.SpringBoot.config;
 public class SpringBootConfig {
 	public static void main(String[] args) {
 		System.out.println("Learning GitHub");
+		System.out.println("Changes done for github");
 	}
 }
